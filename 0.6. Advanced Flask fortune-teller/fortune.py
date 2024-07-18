350582c3d05eb76b0374c1eab907d82d37234304
yasmensh24-meet <yasminshadafna@gmail.com>
2024-07-18 21:29:00 +0300

    done

--- 0.6. Advanced Flask fortune-teller/fortune.py
+++ 0.6. Advanced Flask fortune-teller/fortune.py
@@ -1,19 +1,17 @@
 from flask import Flask,render_template
-import random
+
 app = Flask(__name__) 
 
 @app.route("/home")
 def home():
 	return render_template("home.html")
 
-
-
+qadar=["you are passing cs","you are not passing cs","your stand is winning today","you are not winning today",]
 
 @app.route("/fortune")
 def fortune():
-	fate=["you are passing cs","you are not passing cs","your stand is winning today","you are not winning today","stay hungry","eat iasa food","not sleep bc of cs "]
-	lol=random.choice(fate)
-	return render_template("fortune.html",lol=lol)
+	return render_template("fortune.html",fate=qadar)
+
 
 
 
